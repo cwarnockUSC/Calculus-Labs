718040df2bf3319769f7b58f76bba6d2b4d69ad4
cwarnockUSC <78029922+cwarnockUSC@users.noreply.github.com>
2021-03-31 13:34:15 -0400

    Delete Scripts/solve_for_x directory

--- Scripts/solve_for_x/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='solve_for_x',
-      version='0.0',
-      description='Solving for x in u-Substitution',
-      packages=['solve_for_x'],
-      author_email='cwarnock@math.sc.edu',
-      zip_safe=False)
